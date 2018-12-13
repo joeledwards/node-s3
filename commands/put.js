99286f41ad2c27ae8bba8b0f3eef0b2ed074001e
Joel Edwards <joeledwards@gmail.com>
2018-12-13 10:47:18 -0700

    feat: added the put command

---
+++ commands/put.js
@@ -0,0 +1,123 @@
+module.exports = {
+  command: 'put <bucket> <key> [file]',
+  desc: 'fetch an s3 resource',
+  builder,
+  handler
+}
+
+function builder (yargs) {
+  yargs
+    .positional('bucket', {
+      type: 'string',
+      desc: 'S3 bucket from which to fetch the object',
+      alisa: 'b'
+    })
+    .positional('key', {
+      type: 'string',
+      desc: 'S3 key identifying the object within the bucket',
+      alias: 'k'
+    })
+    .positional('file', {
+      type: 'string',
+      desc: 'basename of the key by default; use "-" to write to stdout',
+      alias: 'f'
+    })
+    .option('stdin', {
+      type: 'boolean',
+      desc: 'stream content to stdin',
+      default: false,
+      alias: 'S'
+    })
+    .option('header', {
+      type: 'array',
+      desc: "headers to add to the S3 object (e.g. -h 'ContentEncoding:gzip')",
+      alias: 'h'
+    })
+}
+
+async function collectStream (stream) {
+  return new Promise((resolve, reject) => {
+    const buffer = []
+    stream.on('data', data => buffer.push(data))
+    stream.once('end', () => resolve(Buffer.concat(buffer)))
+    stream.once('error', reject)
+  })
+}
+
+async function handler ({ bucket, key, file, stdin, header }) {
+  const r = require('ramda') 
+  const fs = require('fs')
+  const aws = require('aws-sdk')
+  const path = require('path')
+
+  const s3 = new aws.S3()
+
+  const splitHeader = header => {
+    if (!header) return
+    if (typeof header !== 'string') return
+
+    const pivot = header.indexOf(':')
+
+    if (pivot < 1) return
+
+    const name = r.trim(header.substr(0, pivot))
+
+    if (name.length < 1) return
+    if (pivot + 1 > header.length) return
+
+    const value = r.trim(header.substr(pivot + 1))
+
+    if (value.length < 1) return
+
+    const pair = {}
+    pair[name] = value
+
+    return pair
+  }
+
+  if (!stdin && !file) {
+    console.error('Must either specify a file or stream to stdin.')
+    process.exit(1)
+  }
+
+  const headers = r.compose(
+    r.filter(r.complement(r.isNil)),
+    r.map(splitHeader),
+    r.filter(r.complement(r.isNil))
+  )(header || [])
+
+  let sourceStream
+  if (stdin) {
+    sourceStream = process.stdin
+  } else {
+    sourceStream = fs.createReadStream(file)
+    console.info(`Putting ${file} to s3://${bucket}/${key} ...`)
+  }
+
+  let body
+  try {
+    // TODO: if file, determine size first
+    // TODO: if stdin, send in 100MB chunks
+    body = await collectStream(sourceStream)
+  } catch (error) {
+    console.error('Error collecting data:', error)
+  }
+
+  const options = {
+    Body: body,
+    Bucket: bucket,
+    Key: key
+  }
+
+  headers.forEach(([name, value]) => options[name] = value)
+
+  const s3Stream = s3.putObject(options, (error, result) => {
+    if (error) {
+      console.error(`Error putting S3 object : ${error}`)
+      process.exit(1)
+    } else {
+      const { ETag: etag } = result
+      console.info(`S3 put complete [ETag:${etag}].`)
+    }
+  })
+}
